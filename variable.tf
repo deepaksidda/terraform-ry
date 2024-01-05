981a5d15bf50af92a0eb18dae9fe851f8c064479
ryb9696 <135820856+ryb9696@users.noreply.github.com>
2024-01-05 15:15:33 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "access_key" {
+  default = "AKIAUA5Y4FO6GIE6NWPP"
+
+}
+variable "secret_key" {
+  default = "WYuIk4jfhxtr15Qctv5ApevIJwzV7Qa4Tqa4L1Tt"
+
+}
+variable "region" {
+  default = "ap-south-1"
+
+}
+variable "ami" {
+  default = "ami-044099ba74ca59bb2"
+
+}
+variable "instance_type" {
+  default = "t2.micro"
+
+}
+variable "key_name" {
+  default = "nave"
+
+}
